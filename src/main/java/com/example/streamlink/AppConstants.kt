e842fdba1f21247748fd908135cccf19a0faaf22
Ahan Sardar <ahansardarvis@gmail.com>
2025-02-20 21:29:20 +0530

    Delete src directory

--- src/main/java/com/example/streamlink/AppConstants.kt
+++
@@ -1,7 +0,0 @@
-package com.example.streamlink
-
-object AppConstants {
-    val appId:Long=2076503410
-    val appSign:String="bb83eaf7c13ef723995731650b51db321be28862d865c3a88251984f94fe0df6"
-
-}
